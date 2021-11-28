7d359f0bd6bec3aa2675801493c98084d20682bd
Maximilian Winkler <maximilian.winkler@tu-bs.de>
2021-11-28 09:40:39 +0100

    Saved successfully as matlab2tikz, but need to choose nicer subplots

---
+++ code/plot_minmax_median_quantiles.m
@@ -0,0 +1,36 @@
+function medianplot_array = plot_minmax_median_quantiles(lineStyle,mins,maxs,medians,quant25s,quant75s,choose_logy,method_array,iter_save,maxiter,minmaxcolor_dict,quantcolor_dict,linecolor_dict,displayname_dict)
+
+      medianplot_array = zeros(1, length(method_array));  % for legend
+      num_iter_array = 1:iter_save:maxiter;
+      num_methods = length(method_array);
+
+      for i = 1:num_methods
+          
+          minmaxcolor_i = minmaxcolor_dict(method_array{i});
+
+          if choose_logy
+            h = fill([num_iter_array  fliplr(num_iter_array)], [log10(maxs(:,i)')  fliplr(log10(mins(:,i))')], minmaxcolor_i,'EdgeColor', 'none');
+            set(h,'facealpha', .5)
+            quantcolor_i = quantcolor_dict(method_array{i});
+            h = fill([num_iter_array  fliplr(num_iter_array)], [log10(quant75s(:,i)')  fliplr(log10(quant25s(:,i))')], quantcolor_i,'EdgeColor', 'none');
+            set(h,'facealpha', .5)
+            medianplot_array(i) = plot( num_iter_array,log10(medians(:,i)),linecolor_dict(method_array{i}),'LineWidth',2,...
+                       'DisplayName',displayname_dict(method_array{i}), 'LineStyle', lineStyle );
+            ylabel('(log scale)')
+          else 
+            h = fill([num_iter_array  fliplr(num_iter_array)], [maxs(:,i)'  fliplr(mins(:,i)')], minmaxcolor_i,'EdgeColor', 'none');
+            set(h,'facealpha', .5)
+            quantcolor_i = quantcolor_dict(method_array{i});
+            h = fill([num_iter_array  fliplr(num_iter_array)], [quant75s(:,i)'  fliplr(quant25s(:,i)')], quantcolor_i,'EdgeColor', 'none');
+            set(h,'facealpha', .5)
+            medianplot_array(i) = plot( num_iter_array,medians(:,i),linecolor_dict(method_array{i}),'LineWidth',2,...
+                       'DisplayName',displayname_dict(method_array{i}), 'LineStyle', lineStyle );          
+          end
+          % use 10^ notation for iterations    
+          %xt = get(gca, 'xtick');
+          %set('xticklabel', sprintf('%1.1e|', xt));
+          
+          axis square
+
+      end
+    end
---
+++ code/plots/noise_5RAc_real_m1000n500_lambda10/driver_experiment.m
@@ -0,0 +1,80 @@
+dir_to_folder_with_figures = 'plots/noise_5RAc_real_m1000n500_lambda10';
+
+m = 1000;  % 500 
+n = 500;  % 200
+sp = 25;   
+
+
+num_repeats = 50;
+
+real_setting = true;
+
+maxiter = 5e5; % Number of iterations % 5e6
+number_data_points = 500;
+iter_save = floor(maxiter/number_data_points);  % each such number of iterations, a data point is added in the error plot
+
+rowsamp = 'rownorms squared';
+colsamp = 'colnorms squared';
+
+lambda_value = 10;  
+
+T_1 = @(z) z;  % gradient gstar for g(x) = 1/2 ||x||_2^2 + gamma ||x||_1
+L_gstar_1 = 1;
+
+% T for Frank's example
+epsilon = 0.01;
+tau = 0.001;
+T_2 = @(z) T_taureg(x,epsilon,tau);
+L_gstar_2 = 1/epsilon + tau;
+
+writeout = false; 
+
+savestep = 1; 
+
+method_array = {'rek', 'srk', 'grek_1'}; 
+
+experiment_description = 'rank-deficient, medium noise in R(A) complement';
+% noise0.5_rangeAc
+
+%experiment_description = 'rank-deficient, large noise in R(A) complement';
+% noise5_rangeAc
+
+%experiment_description = 'rank deficient, noise split into R(A) and R(A) complement';
+
+%experiment_description = 'rank deficient, only noise in R(A) complement, well conditioned A'; % maxiter = 2*1e5
+% with sigma_max = 2, sigma_min = 1 -> very fast with sigma_max = 100,
+% convergence, ESREK best, SREK improves very much tue to change in xhat_min, but ESREK surprisingly not
+
+%experiment_description = 'rank deficient, only noise in R(A) complement, well conditioned A and xhat';
+% with xhat_max = 6, xhat_min = 1 
+% slow convergence, SREK best, ESREK worse than SRK
+
+%experiment_description = 'rank deficient, only noise in R(A) complement, well conditioned A and xhat';  
+% with sigma_max = 2, xhat_min = 0, xhat_max = 5 -> very fast
+% convergence, ESREK not affected
+
+%experiment_description = 'rank deficient, noise split into R(A) and R(A) complement, well conditioned A and xhat';
+
+%experiment_description = 'dctmatrix, noise split into R(A) and R(A) complement';
+
+%experiment_description = 'rank deficient, consistent, no noise';
+
+%experiment_description = 'rank deficient, consistent, no noise';
+
+disp_instance = false;
+
+stopcrit_sample_pars.length_resAbz_sampled = ceil(m/2);
+stopcrit_sample_pars.length_resATz_sampled = ceil(n/2);
+stopcrit_sample_pars.min_possible_iter_for_stopping = 4*max(m,n);
+T = {T_1};
+L_gstar = [L_gstar_1];
+
+data = experiment(n,m,sp,real_setting,lambda_value,T,L_gstar,maxiter,num_repeats,iter_save,rowsamp,colsamp,...
+                  writeout,disp_instance,savestep,stopcrit_sample_pars,method_array,experiment_description);                           
+
+save(fullfile(dir_to_folder_with_figures, 'data.mat'), 'data', '-mat');
+save_figures(dir_to_folder_with_figures)
+                              
+                              
+                              
+                              
